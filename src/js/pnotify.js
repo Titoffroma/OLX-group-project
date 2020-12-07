fac810a6271b9efd92eb096444ccd3a52d3617cd
Titoff Roma <64364502+Titoffroma@users.noreply.github.com>
2020-12-07 23:49:35 +0200

    adds image and pnotify, clears trash

---
+++ src/js/pnotify.js
@@ -0,0 +1,24 @@
+import { error, defaults } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/Material.css';
+import 'material-design-icons/iconfont/material-icons.css';
+defaults.styling = 'material';
+defaults.icons = 'material';
+defaults.hide = false;
+defaults.remove = false;
+delete defaults.stack;
+
+function pushError(err) {
+  error({
+    text: `${err}`,
+    type: 'error',
+    addClass: 'error',
+  });
+}
+
+function removeError() {
+  const errorRef = document.querySelector('.error');
+  if (errorRef) errorRef.remove();
+}
+
+export default { pushError, removeError };
