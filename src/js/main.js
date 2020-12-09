e0d7ff5cf21d045a4c74086da9dd33c83ec51901
Alina Bovdyr <alinabovdyr92@gmail.com>
2020-12-09 17:32:51 +0200

    Добавляет шаблоны карточек создания и редактирования объявления

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,6 @@
 import { load, save, remove } from './storage';
 import { pushError, removeError } from './pnotify';
+
+import editModal from '../templates/edit-card.hbs';
+const bodyPage = document.querySelector('body');
+bodyPage.insertAdjacentHTML('afterbegin', editModal());
