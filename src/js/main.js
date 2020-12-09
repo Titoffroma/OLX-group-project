b49bccdd48210ef66adcbeab599d7931ce3ea0b2
Alina Bovdyr <alinabovdyr92@gmail.com>
2020-12-09 17:33:50 +0200

    Правки

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,3 @@
 import { load, save, remove } from './storage';
 import { pushError, removeError } from './pnotify';
 
-import editModal from '../templates/edit-card.hbs';
-const bodyPage = document.querySelector('body');
-bodyPage.insertAdjacentHTML('afterbegin', editModal());
