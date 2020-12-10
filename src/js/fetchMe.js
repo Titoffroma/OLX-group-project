1720dd7272ee84feb53846130353a678595be928
Titoff Roma <64364502+Titoffroma@users.noreply.github.com>
2020-12-10 12:36:12 +0200

    adds class description and some fixes

--- src/js/fetchMe.js
+++ src/js/fetchMe.js
@@ -1,5 +1,5 @@
 import { load, save, remove } from './storage';
-import { pushError, removeError } from './pnotify';
+import { pushError } from './pnotify';
 
 class FetchMe {
   constructor() {
@@ -10,20 +10,20 @@ class FetchMe {
       sid: '',
     };
     this.points = {
-      reg: '/auth/register',
-      login: '/auth/login',
-      logout: '/auth/logout',
-      refresh: '/auth/refresh',
-      google: '/auth/google',
-      user: '/user',
-      call: '/call',
-      fav: '/call/favourite',
-      myFav: '/call/favourites',
-      myCalls: '/call/own',
+      reg: '/auth/register/',
+      login: '/auth/login/',
+      logout: '/auth/logout/',
+      refresh: '/auth/refresh/',
+      google: '/auth/google/',
+      user: '/user/',
+      call: '/call/',
+      fav: '/call/favourite/',
+      myFav: '/call/favourites/',
+      myCalls: '/call/own/',
     };
     this.headers = {
       'Content-Type': 'application/json',
-      authorization: this.token.accessToken,
+      authorization: '',
     };
   }
   get token() {
@@ -39,11 +39,15 @@ class FetchMe {
   logout() {
     const opt = { point: this.points.logout, method: 'POST', logout: true };
     this.getRequest(opt);
-    // console.log('deleted', load('Token').sid);
+    remove('Token');
+    this.token = {
+      accessToken: '',
+      refreshToken: '',
+      sid: '',
+    };
   }
   async login(opt) {
     return await this.getRequest(opt).then(data => {
-      // console.log('saved', data);
       this.token = data;
       save('Token', data);
       return data;
@@ -61,17 +65,13 @@ class FetchMe {
       headers: this.headers,
     };
     if (body) opt.body = JSON.stringify(body);
-    if (!this.token.accessToken.length) {
-      if (load('Token')) opt.headers.authorization = load('Token').accessToken;
-    }
+    if (load('Token')) opt.headers.authorization = load('Token').accessToken;
     const par = logout ? false : opt;
-
     const url = this.URL + point + query;
     return await this.sendRequest(url, par);
   }
   async sendRequest(url, opt) {
     try {
-      console.log(opt);
       if (!opt)
         return fetch(url, {
           method: 'POST',
@@ -87,7 +87,7 @@ class FetchMe {
       }
       return await response.json();
     } catch (err) {
-      console.log('mistake', err);
+      console.log('mistake', err.message);
     }
   }
   async refresh(url, opt) {
@@ -102,7 +102,6 @@ class FetchMe {
     try {
       const response = await fetch(this.URL + this.points.refresh, option);
       response.json().then(data => {
-        console.log('new Toc', data);
         this.token = data;
         save('Token', this.token);
       });
@@ -112,7 +111,51 @@ class FetchMe {
     }
   }
 }
-
 const fetchFunctions = new FetchMe();
-
 export default fetchFunctions;
+
+// Небольшая инструкция
+// ///////1/ Перед отправкой запросов нужно создать объект для body запроса,
+// если этого требует API(для метода 'GET' не нужно).
+// Напрмер для логина в body отправляется такой объект:
+const logInfo = {
+  email: 'titoff.roma@gmail.com',
+  password: 'qweqwe12',
+};
+// //////2/ Затем нужно создать объект параметров запроса, он включает в себя до 4 параметров,
+// в зависимости от запроса(как в доках API)
+// point - обязательный параметр, он указывает на какой адрес отправлять запрос
+// Все point для нашего API указаны к конструкторе класса - в свойстве points
+// Например для логина создается такой объект параметров запроса
+const request = {
+  point: fetchFunctions.points.login,
+  body: logInfo,
+  method: 'POST',
+  //query: '1'
+};
+// Например для для получения данных пользователя создается такой объект параметров запроса
+const newRequest = {
+  point: fetchFunctions.points.user,
+};
+// //////3/ Чтоб получить информацию с бэкэнда в классе имеются 3 метода:
+//
+async function some() {
+  let response = await fetchFunctions.login(request);
+
+  console.log('login -', response);
+
+  response = fetchFunctions.logout();
+
+  console.log('logout - всегда вернет undefined - ', response);
+
+  response = await fetchFunctions.getRequest(newRequest);
+
+  console.log('get user - unauthorized -', response);
+
+  response = await fetchFunctions.login(request);
+
+  response = await fetchFunctions.getRequest(newRequest);
+
+  console.log('get user - authorized -', response);
+}
+some();
