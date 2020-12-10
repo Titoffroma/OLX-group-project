c22ee51987b033a7ebc123c7f56a8da5e2635649
OleksandrGrubyi <ghrubyi@ukr.net>
2020-12-10 16:13:00 +0200

    add popup hbs

--- src/index.js
+++ src/index.js
@@ -4,15 +4,6 @@ import 'material-design-icons/iconfont/material-icons.css';
 
 import './js/menu.js';
 
-//import modal from './js/open-modal-class'
-
-import renderCardlist from './templates/pop-up-exit-confirm.hbs';
-import popup from './templates/pop-up-search.hbs'
-
-
-
-const hbsFunctions = [renderCardlist, popup];
-
 import './js/authorization';
 import './js/menu.js';
 import './js/fetchMeDescription';
