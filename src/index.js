a6bcdd3957f438cd4bc2b89b2f20821bc1958de1
OleksandrGrubyi <ghrubyi@ukr.net>
2020-12-12 00:29:30 +0200

    add openmodal

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import './js/main.js';
 import 'material-design-icons/iconfont/material-icons.css';
 import './js/menu.js';
 import './js/fetchMeDescription';
-import './js/authorization';
+//import './js/authorization';
