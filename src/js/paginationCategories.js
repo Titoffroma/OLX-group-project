aac29ae1e1f617ef9bb9a516cfab47f32131b9ae
Titoff Roma <64364502+Titoffroma@users.noreply.github.com>
2020-12-13 00:47:34 +0200

    adds main pege logics

--- src/js/paginationCategories.js
+++ src/js/paginationCategories.js
@@ -1,64 +1,49 @@
 import fetchFunctions from './fetchMe';
 import renderCategories from '../templates/category.hbs';
-import renderFilter from '../templates/filter.hbs';
-import renderList from '../templates/watchAll.hbs';
-
-const refs = {
-  categoryList: document.querySelector('.categorylist'),
-  pagination: document.querySelector('[data-pagination]'),
-  watchAll: document.querySelector('.categorylist__link'),
-};
+import renderPagination from '../templates/pagination.hbs';
 
 appPage();
-refs.pagination.addEventListener('click', onPaginationPage);
 
-// {
-//   once: true;
-// }
 async function appPage() {
   const searchQuery = {
     point: fetchFunctions.points.call,
     query: '?page=1',
   };
-
   const searchResult = await fetchFunctions.getRequest(searchQuery);
-  const orderedSearch = renderCategories(searchResult);
-  document.querySelector('section.categories').innerHTML = orderedSearch;
+  const orderedSearch = renderPagination(searchResult);
+  document.querySelector('main div.container').innerHTML = orderedSearch;
+  document.body.addEventListener('click', onPaginationPage, { once: true });
 }
 
 async function onPaginationPage(event) {
-  event.preventDefault();
-
-  if (event.target.nodeName !== 'A') {
-    return;
+  document.body.removeEventListener('click', onPaginationPage, { once: true });
+  if (event.target.hasAttribute('data-clear-filter')) {
+    return appPage();
   }
-
-  const currentActivePage = refs.pagination.querySelector('.active');
-  if (currentActivePage) {
-    currentActivePage.classList.remove('active');
+  if (event.target.classList.contains('pagination__link')) {
+    const pagination = document.querySelector('div[data-pagination]');
+    event.preventDefault();
+    if (event.target.nodeName !== 'A') {
+      return;
+    }
+    const currentActivePage = pagination.querySelector('.active');
+    if (currentActivePage) {
+      currentActivePage.classList.remove('active');
+    }
+    const currentPage = event.target;
+    currentPage.classList.add('active');
+    const numderPage = event.target.textContent;
+    const searchQuery = {
+      point: fetchFunctions.points.call,
+      query: `?page=${numderPage}`,
+    };
+    const searchResult = await fetchFunctions.getRequest(searchQuery);
+    const orderedSearch = renderCategories(searchResult);
+    document.querySelector('section.categories').innerHTML = orderedSearch;
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
   }
-
-  const currentPage = event.target;
-  currentPage.classList.add('active');
-
-  const numderPage = event.target.textContent;
-  const searchQuery = {
-    point: fetchFunctions.points.call,
-    query: `?page=${numderPage}`,
-  };
-
-  const searchResult = await fetchFunctions.getRequest(searchQuery);
-  const orderedSearch = renderCategories(searchResult);
-
-  document.querySelector('section.categories').innerHTML = orderedSearch;
-
-  console.log(searchResult);
+  document.body.addEventListener('click', onPaginationPage, { once: true });
 }
-
-function openItemsByFilter() {
-  document.querySelector('section.categories').innerHTML = renderList(
-    searchResult,
-  );
-}
-
-export default { onPaginationPage };
