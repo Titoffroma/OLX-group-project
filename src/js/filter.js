103b54243fb4e3b0f2c2d57a1e3c1a2db1bccf9e
Titoff Roma <64364502+Titoffroma@users.noreply.github.com>
2020-12-13 01:17:10 +0200

    fix filters and categories

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,119 +1,27 @@
+import hbsFunction from '../templates/filter.hbs';
+import renderCards from '../templates/cardset.hbs';
+import fetchFunctions from './fetchMe.js';
 
-import fetchFunctions from './fetchMe';
-import renderFilter from '../templates/filter.hbs';
-import renderList from '../templates/cardset.hbs';
-import renderCategories from '../templates/category.hbs';
-import onPaginationPage from './paginationCategories';
-
-openFilterHeader();
-
-const refs = {
-  filter: document.querySelector('.header_filter'),
-  btnFilter: document.querySelector('.header_filter_button'),
-  pagination: document.querySelector('.pagination'),
-  cardSet: document.querySelectorAll('.categorylist__description'),
-};
-
-refs.btnFilter.addEventListener('click', clearFilter, {
-  once: true,
-});
-
-refs.filter.addEventListener('click', openCardsByFilter, {
-  once: true,
-});
-
-refs.cardSet.addEventListener('click', openCards, {
-  once: true,
-});
-
-async function openFilterHeader() {
-  const searchCategories = {
+async function renderFilter() {
+  const filterUL = document.querySelector('.header_filter');
+  const clearFilterRef = document.querySelector('.header_filter_button');
+  const filterRequest = {
     point: fetchFunctions.points.cat,
   };
-
-  const categoriesResult = await fetchFunctions.getRequest(searchCategories);
-
-  const filterCategories = renderFilter(categoriesResult);
-  document.querySelector('.header_filter').innerHTML = filterCategories;
+  const response = await fetchFunctions.getRequest(filterRequest);
+  filterUL.innerHTML = hbsFunction(response);
+  document.body.addEventListener('click', Mycallback);
 }
+renderFilter();
 
-async function clearFilter() {
-  refs.btnFilter.addEventListener('click', clearFilter, {
-    once: true,
-  });
-
-  const currentFilter = refs.filter.querySelector('.active');
-  if (currentFilter) {
-    currentFilter.classList.remove('active');
+async function Mycallback(event) {
+  if (event.target.hasAttribute('data-filter')) {
+    event.preventDefault();
+    const request = {
+      point: fetchFunctions.points.catCalls,
+      query: event.target.dataset.filter,
+    };
+    const response = await fetchFunctions.getRequest(request);
+    document.querySelector('div.container').innerHTML = renderCards(response);
   }
 }
-
-async function openCardsByFilter(event) {
-  refs.filter.addEventListener('click', openCardsByFilter, {
-    once: true,
-  });
-
-  event.preventDefault();
-  //refs.pagination.classList.add('visually-hidden');
-
-  if (event.target.nodeName !== 'A') {
-    return;
-  }
-
-  const controlActiveFilter = refs.filter.querySelector('.active');
-  if (controlActiveFilter) {
-    controlActiveFilter.classList.remove('active');
-  }
-  const currentFilter = event.target;
-  currentFilter.classList.add('active');
-
-  const currentActivePage = refs.pagination.querySelector('.active');
-
-  //currentActivePage.classList.remove('active');
-
-  const category = currentFilter.textContent;
-  // const numderPage = currentPage.textContent;
-  const searchCards = {
-    point: fetchFunctions.points.catCalls + `${category}`,
-    // query: `?page=${numderPage}`,
-  };
-  const cardsResult = await fetchFunctions.getRequest(searchCards);
-  const filterCards = renderList(cardsResult);
-  document.querySelector('section.categories').innerHTML = filterCards;
-}
-
-async function openCards(event) {
-  refs.cardSet.addEventListener('click', openCards, {
-    once: true,
-  });
-
-  event.preventDefault();
-  //refs.pagination.classList.add('visually-hidden');
-
-  if (event.target.nodeName !== 'A') {
-    return;
-  }
-
-  //   const controlActiveFilter = refs.filter.querySelector('.active');
-  //   if (controlActiveFilter) {
-  //     controlActiveFilter.classList.remove('active');
-  //   }
-  const currentFilter = event.target;
-  //currentFilter.classList.add('active');
-  console.log(currentFilter);
-  //const currentActivePage = refs.pagination.querySelector('.active');
-
-  //currentActivePage.classList.remove('active');
-
-  const category = currentFilter.textContent;
-  // const numderPage = currentPage.textContent;
-  const searchCards = {
-    point: fetchFunctions.points.catCalls + `${category}`,
-    // query: `?page=${numderPage}`,
-  };
-  const cardsResult = await fetchFunctions.getRequest(searchCards);
-  const filterCards = renderList(cardsResult);
-  document.querySelector('section.categories').innerHTML = filterCards;
-}
-
-export { openFilterHeader, clearFilter, openCardsByFilter };
--- src/js/paginationCategories.js
+++ src/js/paginationCategories.js
@@ -1,19 +1,8 @@
 import fetchFunctions from './fetchMe';
 import renderCategories from '../templates/category.hbs';
-
-import renderFilter from '../templates/filter.hbs';
-import renderList from '../templates/cardset.hbs';
+import renderPagination from '../templates/pagination.hbs';
 
 appPage();
-const refs = {
-  categoryList: document.querySelector('.categorylist'),
-  pagination: document.querySelector('[data-pagination]'),
-  cardSet: document.querySelectorAll('.categorylist__description'),
-};
-
-refs.pagination.addEventListener('click', onPaginationPage, {
-  once: true,
-});
 
 async function appPage() {
   const searchQuery = {
@@ -27,12 +16,8 @@ async function appPage() {
 }
 
 async function onPaginationPage(event) {
-
-  //event.preventDefault();
-
-  if (event.target.nodeName !== 'A') {
-    return;
-
+  if (event.target.hasAttribute('data-clear-filter')) {
+    return appPage();
   }
   if (event.target.classList.contains('pagination__link')) {
     const pagination = document.querySelector('div[data-pagination]');
@@ -59,33 +44,5 @@ async function onPaginationPage(event) {
       behavior: 'smooth',
     });
   }
-
-
-  const currentPage = event.target;
-
-  const numderPage = currentPage.textContent;
-  currentPage.classList.add('active');
-
-  const searchQuery = {
-    point: fetchFunctions.points.call,
-    query: `?page=${numderPage}`,
-  };
-
-  const searchResult = await fetchFunctions.getRequest(searchQuery);
-  const orderedSearch = renderCategories(searchResult);
-  toScroll();
-  document.querySelector('section.categories').innerHTML = orderedSearch;
-  refs.pagination.addEventListener('click', onPaginationPage, {
-    once: true,
-  });
-}
-
-export default { onPaginationPage, appPage };
-
-function toScroll() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  });
-
+  document.body.addEventListener('click', onPaginationPage, { once: true });
 }
