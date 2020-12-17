7ea952515aff1b39d7d172ce3543fe01e345178e
Roman-Denysenko <roman.denisenko83@gmail.com>
2020-12-17 03:32:15 +0200

    Редактирует фото в модалке

--- src/js/editProduct.js
+++ src/js/editProduct.js
@@ -1,7 +1,8 @@
 import fetchFunctions from './fetchMe';
 import editCardHbs from '../templates/edit-card.hbs';
 import modalLogic from './addAndEditModalLogic';
-//import fetchProduct from './productModal';
+import { load, save, remove } from './storage';
+import { pushError } from './pnotify';
 
 
 async function productFetch(id, title) {
@@ -17,92 +18,99 @@ async function productFetch(id, title) {
   });
 }
 
- 
-
 export default async function openEditCard(evt) {
-    const id = evt.target.getAttribute('data-changeid');
-    const title = evt.target.getAttribute('data-title');
-    const data = await productFetch(id, title);
-    
+  const id = evt.target.getAttribute('data-changeid');
+  const title = evt.target.getAttribute('data-title');
+  const data = await productFetch(id, title);
+
   const markup = editCardHbs(data);
+
   
 
-  document.body.addEventListener('click', () => { 
+  function onEditCard(e) {
+    modalLogic();
+     e.preventDefault();
+  const photoElem = document.querySelector('#photoElem');
+  const cardForm = document.querySelector('.add-card__form');
+  const closeBtn = document.querySelector('span[data-close]');
+  const formData = new FormData();
+  const myHeaders = new Headers();
+  
+    document.body.addEventListener('click', () => {
     if (ref.btnChekRef.checked === true) {
       document.querySelector(`.on-change`).textContent = 'Видалити';
     } else {
       document.querySelector(`.on-change`).textContent = 'Зберегти';
     }
-
-   });
-
-    function onEditCard(e) {
-     // e.preventDefault();
-      modalLogic();
-      const refs = {
-        checkboxEl: document.querySelector('.checkbox-field'),
-        submitBtn: document.querySelector('.on-change'),
-        addCardForm: document.querySelector('.add-card__form'),
-        photo: document.querySelector('#photoElem'),
-      };
-      
-    
-      const formElements = e.target.elements;
-      const title = formElements.title.value;
-      const description = formElements.description.value;
-      const category = formElements.category.value;
-      const price = formElements.price.value;
-      const phone = formElements.phone.value;
-    
-      const changedFormData = {
-        title,
-        description,
-        category,
-        price,
-        phone,
-       // file,
-      };
-
-    const formData = new FormData();
-      refs.photo.addEventListener('change', () => {
-      })
-
-
-      if (!e.target.elements.checkbox.checked) {
-        const patchRequest = {
-          point: fetchFunctions.points.call,
-          body: changedFormData,
-          redirect: 'follow',
-          method: 'PATCH',
-          query: id,
-        }
-        async function patchCard() {
-          let response = await fetchFunctions.getRequest(patchRequest);
-        }
-        patchCard()
+  });
+    cardForm.addEventListener('submit', onFormSubmit);
+    photoElem.addEventListener('change', function () {
+      if (!photoElem.files[0]) {
+        return;
       } else {
-        const deleteRequest = {
-          point: fetchFunctions.points.call,
-          method: 'DELETE',
-          redirect: 'follow',
-          query: id,
-                
-        }
-        async function deleteCard() {
-          let response = await fetchFunctions.getRequest(deleteRequest);
-        }
-        deleteCard()
-      }
+       formData.set('file', photoElem.files[0]);
+    }
+    });
+      
+    async function onFormSubmit(e) {
+      e.preventDefault()
+        if (!e.target.elements.checkbox.checked) {
+    const formElements = e.currentTarget.elements;
+    const title = formElements.title.value;
+    const description = formElements.description.value;
+    const category = formElements.category.value;
+    const price = formElements.price.value;
+    const phone = formElements.phone.value;
+
+    formData.set('title', title);
+    formData.set('description', description);
+    formData.set('category', category);
+    formData.set('price', Number(price));
+    formData.set('phone', phone);
+    myHeaders.append('Authorization', `Bearer ${load('Token').accessToken}`);
+    const URL = `https://callboard-backend.herokuapp.com/call/${id}`;
+    const requestOptions = {
+      method: 'PATCH',
+      redirect: 'follow',
+      headers: myHeaders,
+      body: formData,
     };
 
-  document.body.addEventListener('submit', onEditCard);
+    const answer = await fetch(URL, requestOptions);
+    if (answer.ok) {
+      closeBtn.click();
+      pushError('Ваше оголошення успішно редаговане');
+    }
+
+    const error = await answer.json();
+    if (error.message) {
+     pushError (error.message);
+      }
+       } else {
+      const deleteRequest = {
+        point: fetchFunctions.points.call,
+        method: 'DELETE',
+        redirect: 'follow',
+        query: id,
+
+      }
+      async function deleteCard() {
+        let response = await fetchFunctions.getRequest(deleteRequest);
+      }
+      deleteCard()
+    }
+  }
+   
+  };
+
+ 
+  document.body.addEventListener('click', onEditCard, { once: true });
+  
   const ref = {
     get btnChekRef() {
       return document.querySelector(`.checkbox`);
     },
   };
   return markup;
-  
-  
 };
 
