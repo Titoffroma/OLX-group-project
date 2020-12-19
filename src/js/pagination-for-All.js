19c8b4cec6e66527e07d402a12fbce1bfc6a70bf
Olena Kaliuzhna <om.kaliuzhna.dz@gmail.com>
2020-12-19 18:15:33 +0200

    fix team-modal and adds styles for pagination-for-all

--- src/js/pagination-for-All.js
+++ src/js/pagination-for-All.js
@@ -1,6 +1,6 @@
 
 import renderPaginationAll from '../templates/pegination-for-All.hbs';
-
+import renderCards from '../templates/cardset.hbs';
 const onPage = 16;
 let dataM;
 
@@ -14,21 +14,23 @@ export default async function paginationAll(data) {
     document.querySelector('main div.container').innerHTML = await renderPaginationAll(markup);
     console.log(markup.length);
     for (let i = 0; i < pages; i += 1) {
-        document.querySelector('.pagination-all').innerHTML += `<button class="btn-pag" data-from=${i} data-to=${i * pages + pages}>${i + 1}</button>`;
+        document.querySelector('main div.container .pagination-all').innerHTML += `<button class="btn-pag" data-from=${i} data-to=${i * pages + pages}>${i + 1}</button>`;
     }
     const pagination = document.querySelector('.pagination-all');
+    
+    // pagination.querySelector('.active').classList.remove('active');
+    //  event.target.classList.add('active');
     pagination.firstElementChild.classList.add('active');
-
     document.body.addEventListener('click', onPaginationAllPage)
 };
    
 async function onPaginationAllPage(event) {
-    if (event.target.classList.contains('btn-pag')) {   
-        const pagination = document.querySelector('.pagination-all');
-        const currentActivePage = pagination.querySelector('.active');
+    if (event.target.classList.contains('btn-pag')) {           
+        const currentActivePage = document.body.querySelector('.btn-pag.active');
         if (currentActivePage) {
          currentActivePage.classList.remove('active');
         }
+
         const currentPage = event.target;        
         currentPage.classList.add('active');
         console.log(currentPage);
@@ -37,7 +39,7 @@ async function onPaginationAllPage(event) {
         const to = (currentPage.dataset.to  - 1)  * onPage;
         const sliced = dataM.slice(from, to );
        
-        document.querySelector('section.categories').innerHTML = await renderPaginationAll(sliced);
+        document.querySelector('section.categories').innerHTML = await renderCards(sliced);
        
         window.scrollTo({
         top: 0,
